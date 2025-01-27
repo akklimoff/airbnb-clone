4cde9aa4d61ff49377ba17aaba77172ca283408b
Aktan Klimov <aktan.klimov2@gmail.com>
2025-01-27 19:52:21 +0600

    new files, categories model and service methods

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {ButtonModule} from 'primeng/button';
+import {FaIconLibrary} from '@fortawesome/angular-fontawesome';
+import {fontAwesomeIcons} from './shared/font-awesome-icons';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ButtonModule],
   templateUrl: './app.component.html',
+  standalone: true,
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'airbnb-clone-front';
+export class AppComponent implements OnInit {
+  faIconLibrary = inject(FaIconLibrary)
+
+  ngOnInit(): void {
+    this.initFontAwesome();
+  }
+
+
+  private initFontAwesome() {
+    this.faIconLibrary.addIcons(...fontAwesomeIcons);
+  }
 }
